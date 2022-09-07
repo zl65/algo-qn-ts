73d4455a3d5555c5991c0c3b15a06a4c98a11183
leler <zhangle3254@gmail.com>
2022-09-07 23:49:56 +0800

    9.7

--- al-classic/26.ts
+++ al-classic/26.ts
@@ -1,36 +1,38 @@
 function isSubStructure(A: TreeNode | null, B: TreeNode | null): boolean {
-  function _isPartlySame(A: TreeNode, B: TreeNode): boolean {
-    const qa = [A]
-    const qb = [B]
+  function dfs(A: TreeNode | null, B: TreeNode | null): boolean {
+    if (B === null) return true
+    if (A === null || A.val !== B.val) return false
+    return dfs(A.left, B.left) && dfs(A.right, B.right)
+  }
+
+  function bfs(A: TreeNode | null, B: TreeNode | null): boolean {
+    const qa: (TreeNode | null)[] = [A]
+    const qb: (TreeNode | null)[] = [B]
+
     while (qb.length > 0) {
       const size = qb.length
       for (let i = 0; i < size; i++) {
-        const rootb = qb.shift()
-        const roota = qa.shift()
-        if (roota && rootb && roota.val === rootb.val) {
-          if (
-            roota.left !== null &&
-            rootb.left !== null &&
-            roota.right !== null &&
-            rootb.right !== null
-          ) {
-            qa.push(rootb.left)
-            qb.push(rootb.left)
-            qa.push(rootb.right)
-            qb.push(rootb.right)
-          }
+        const rootB = qb.shift()
+        const rootA = qa.shift()
+        if (rootA === null && rootB === null) {
+          continue
+        } else if (rootA && rootB && rootA.val === rootB.val) {
+          qa.push(rootA.left)
+          qb.push(rootB.left)
+          qa.push(rootA.right)
+          qb.push(rootB.right)
+        } else {
+          return false
         }
-        return false
       }
     }
     return true
   }
+
   function _isSubStructure(A: TreeNode | null, B: TreeNode | null): boolean {
     if (A === null || B === null) return false
     return (
-      _isPartlySame(A, B) ||
-      _isSubStructure(A?.left!, B) ||
-      _isSubStructure(A?.right!, B)
+      dfs(A, B) || _isSubStructure(A.left, B) || _isSubStructure(A.right, B)
     )
   }
   return _isSubStructure(A, B)
